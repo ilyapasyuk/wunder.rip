843f820b5a7819632a4ffa51854f2ae2a35b6d0a
ilyapasyuk <pasyuk.com@gmail.com>
2020-05-05 14:53:54 +0300

    add social auth

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    <div className="logo" />
+    <p>
+      WunderTodo
+    </p>
+  </footer>
+);
+
+export default Footer;
