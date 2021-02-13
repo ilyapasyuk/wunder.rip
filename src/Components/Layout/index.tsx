6ab959e128c628f446d79dc2ad3e3704d4083b8e
Ilya Pasuyk <pasyuk.com@gmail.com>
2021-02-13 23:56:42 +0300

    Apply suggestions from code review

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -11,8 +11,6 @@ import { TodoList } from 'Components/TodoList'
 import { GlobalStyle, StyledAddTask, StyledLayout, StyledTodos } from './style'
 import { Todo } from '../Todo'
 
-console.log(process.env)
-
 export type User = {
     id: string
     avatar: string
